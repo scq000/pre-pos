a888e1d9d5c7c45ebe9d23849e263a6c194e1fe5
scq000 <scq000@hotmail.com>
2015-06-28 11:03:50 +0800

    solve section-1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,3 +1,10 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  for(var index in collection_a){
+    if(collection_b.indexOf(collection_a[index]) !== -1){
+      result.push(collection_a[index]);
+    }
+  }
+  return result;
 }

31bb086a9942c92cc8132dcdd5f3c8d2ff7050c9
scq000 <scq000@hotmail.com>
2015-06-28 11:21:46 +0800

    solve all the section-1

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,3 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var collection_b = collection_b.join("").split(",");
+  var result = [];
+  for(var index in collection_a){
+    if(collection_b.indexOf(collection_a[index]) !== -1){
+      result.push(collection_a[index]);
+    }
+  }
+  return result;
 }
--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,3 +1,11 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  collection_b = object_b.value;
+  for(var index in collection_a){
+    if(collection_b.indexOf(collection_a[index]) !== -1){
+      result.push(collection_a[index]);
+    }
+  }
+  return result;
 }
